e38d8b3adef617a0de6389c7ad7c6c385986144c
white-black66 <63218402+white-black66@users.noreply.github.com>
2021-04-26 17:29:25 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,16 @@
+
+from pspnet import PSPNet
+from PIL import Image
+
+pspnet = PSPNet()
+
+while True:
+    img = input('Input image filename:')
+    try:
+        image = Image.open(img)
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = pspnet.detect_image(image)
+        r_image.show()
